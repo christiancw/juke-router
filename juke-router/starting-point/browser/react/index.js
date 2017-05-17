db3919a0cd39fa558f0e666c9ae694b52d98c64c
Christian Wentling <christianwentling@gmail.com>
2017-05-17 12:33:38 -0400

    with minor changes to child routes

--- juke-router/starting-point/browser/react/index.js
+++ juke-router/starting-point/browser/react/index.js
@@ -17,8 +17,8 @@ ReactDOM.render(
       <Route path='/albums/:albumId' component={Album} />
       <Route path='/artists' component={Artists} />
       <Route path='/artists/:artistId' component={Artist} >
-        <Route path='/artists/:artistId/albums' component={Albums}/>
-        <Route path='/artists/:artistId/songs' component={Songs}/>
+        <Route path='albums' component={Albums}/>
+        <Route path='songs' component={Songs}/>
       </Route>
     </Route>
   </Router>,
